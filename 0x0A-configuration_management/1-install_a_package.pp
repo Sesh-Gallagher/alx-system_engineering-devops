54f69e4fcad63374c8fa6c51374a5169fb6cc5f0
Sesh-Gallagher <t.ntwana@gmail.com>
2024-07-03 16:19:34 -0700

    installing a pckage

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 # install flask from pip3. Using Puppet
 
-package { 'puppet-lint':
+package { 'flask':
+  ensure   =>  '2.1.0',
+  provider => 'pip3',
+}
+
+packege { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
   ensure   => '2.1.1',
-  provider => 'gem',
+  provider => 'pip3',
+  require  => package['Flask'],
 }
