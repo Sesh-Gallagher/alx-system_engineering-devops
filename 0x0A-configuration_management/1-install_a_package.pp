3e9b0ed4dfb258c80efb0e1c481bcf1cc6a2c0bf
Sesh-Gallagher <t.ntwana@gmail.com>
2024-07-03 16:32:08 -0700

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,15 @@
 # install flask from pip3. Using Puppet
 
-package { 'flask':
-  ensure   =>  '2.1.0',
-  provider => 'pip3',
-}
-
 packege { 'python3.8':
   ensure   => '3.8.10',
   provider => 'pip3',
 }
+
+package { 'Flask':
+  ensure   =>  '2.1.0',
+  provider => 'pip3',
+}
+
 
 package { 'werkzeug':
   ensure   => '2.1.1',
