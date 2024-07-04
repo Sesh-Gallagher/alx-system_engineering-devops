e8a48ba031c1248d65d90177266c900ce36ddf52
Sesh-Gallagher <t.ntwana@gmail.com>
2024-07-03 17:30:16 -0700

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,19 @@
 # Using puppet script to connect without password and create ssh file
 
-# define the type of file
-file { '/etc/ssh/ssh_config':
-  ensure => present,
-}
+include stdlib
+
 file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config'
-  line    => 'PasswordAuthentication no',
-  match   => '^#PasswordAuthentication',
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
+
 }
 file_line { 'Declare identity file':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^#IdentityFile',
-}
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
