6928a4928653539fe88d5179539cff733ece9474
Sesh-Gallagher <t.ntwana@gmail.com>
2024-07-03 17:07:27 -0700

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# installs flask from pip3, using puppet
-
+# installs the package puppet-lint using puppet
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
