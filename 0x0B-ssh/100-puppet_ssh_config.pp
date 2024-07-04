78f27092a2c75be9fd56f259bdc52f864744fcd8
Sesh-Gallagher <t.ntwana@gmail.com>
2024-07-03 17:41:25 -0700

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,9 @@
-# Using puppet script to connect without password and create ssh file
+# using puppet to make changes to the default ssh config file
+# so that one can connect to a server without typing a password.
 
 include stdlib
 
-file_line { 'Turn off passwd auth':
+file_line { 'Turn off password auth':
   path               => '/etc/ssh/ssh_config',
   line               => '    IdentityFile ~/.ssh/school',
   match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
@@ -10,10 +11,11 @@ file_line { 'Turn off passwd auth':
   append_on_no_match => true
 }
 
-}
-file_line { 'Declare identity file':
+
+file_line { 'Deny Password Auth':
   path               => '/etc/ssh/ssh_config',
   line               => '    PasswordAuthentication no',
   match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
   replace            => true,
   append_on_no_match => true
+}
