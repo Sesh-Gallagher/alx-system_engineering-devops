e01c50e007ce08bbd7864397c78ef1af51fce3ae
Sesh-Gallagher <t.ntwana@gmail.com>
2024-07-12 20:00:03 -0700

    installation of flask using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 # create a file in /tmp,using puppet
 
-file { '/tmp/school':
+file {'Sesh_Gallagher':
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
