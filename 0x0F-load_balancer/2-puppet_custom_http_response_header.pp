1d838161d0f9995128cc43e58f1fc5d50462877f
Sesh-Gallagher <t.ntwana@gmail.com>
2024-07-13 12:56:00 -0700

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,21 +2,30 @@
 # custom HTTP header must be X-Served-By
 
 
-exec { 'update':
+exec { 'update system':
         command => '/usr/bin/apt-get update',
 }
 
--> package { 'nginx':
-	ensure => 'present',
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
--> file {'http_header':
-	path => '/etc/nginx/nginx.conf',
-	match => 'http {',
-	line => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
--> exec {'redirect_me':
-	command => '/usr/sbin/service nginx restart',
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://wastedpotential.tk/ permanent;" /etc/nginx/sites-available/default',
 	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
